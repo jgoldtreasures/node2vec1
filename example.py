36d740cc93c8e076c4dfdf969542c6dca0cc5c09
Julian Robinson <jer171@case.edu>
2020-09-15 11:53:00 -0400

    Changed output directories and changed input to be a .edgelist file

--- example.py
+++ example.py
@@ -2,14 +2,19 @@ import networkx as nx
 from node2vec import Node2Vec
 
 # FILES
-EMBEDDING_FILENAME = './embeddings.emb'
-EMBEDDING_MODEL_FILENAME = './embeddings.model'
+EMBEDDING_FILENAME = './embeddings/embeddings.emb'
+EMBEDDING_MODEL_FILENAME = './embeddings/embeddings.model'
+EDGELIST_INPUT = './edgelist/Biogrid.edgelist'
 
 # Create a graph
-graph = nx.fast_gnp_random_graph(n=100, p=0.5)
+# graph = nx.fast_gnp_random_graph(n=100, p=0.5)
+
+graph = nx.read_edgelist(EDGELIST_INPUT, nodetype=int, create_using=nx.DiGraph())
+# for edge in graph.edges():
+#     graph[edge[0]][edge[1]]['weight'] = 1
 
 # Precompute probabilities and generate walks
-node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=4)
+node2vec = Node2Vec(graph, dimensions=64, walk_length=30, num_walks=200, workers=1)
 
 ## if d_graph is big enough to fit in the memory, pass temp_folder which has enough disk space
 # Note: It will trigger "sharedmem" in Parallel, which will be slow on smaller graphs
